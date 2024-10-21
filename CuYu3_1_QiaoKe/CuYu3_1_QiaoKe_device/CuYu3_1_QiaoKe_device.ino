3b5ef3b47ec4aa5097b0ea9449f71fe516d9098e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-10-21 15:38:58 +0900

    2x2 version done

---
+++ CuYu3_1_QiaoKe/CuYu3_1_QiaoKe_device/CuYu3_1_QiaoKe_device.ino
@@ -0,0 +1,379 @@
+// original: ESPNOW sample code
+// modified by Nyanyan
+
+/**
+   ESPNOW - Basic communication - Slave
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
+   Description: This sketch consists of the code for the Slave module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+
+   << This Device Slave >>
+
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave
+
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Mozzi.h>
+//#include <MozziGuts.h>
+//#include <ReverbTank.h>
+#include <Oscil.h>
+#include <tables/saw2048_int8.h>
+#include <ADSR.h>
+#include <ResonantFilter.h>
+
+#define CHANNEL 1
+
+#define N_FACES 6
+#define N_TONES 5
+#define CONTROL_RATE 128
+
+#define DATA_CHARGING 0b01000000
+#define CHARGING_LED 33
+
+#define DATA_DEEPSLEEP 0b10000000
+#define AWAKE_LED 5
+bool awake_state = true;
+
+const int rgb_led[3] = {27, 14, 32}; // RGB
+const int tone_led_colors[N_TONES][3] = { // RGB, false: off
+  { true,  true,  true}, // 呂旋法 (黒->白)
+  { true, false, false}, // 律旋法 (橙->赤)
+  {false,  true, false}, // 民謡音階 (緑->緑)
+  { true,  true, false}, // 都節音階 (黄->黄)
+  {false, false,  true}, // 琉球音階 (青->青)
+};
+
+int led_blink_state = 1;
+
+int tone_idx = 0;
+
+const float tones[N_TONES][N_FACES] = {
+  {
+    // 呂旋法
+    391.995f, // W G4 4
+    261.626f, // Y C4 1
+    329.628f, // G E4 3
+    523.251f, // B C5 6
+    293.665f, // R D4 2
+    440.000f  // O A4 5
+  },
+  {
+    // 律旋法
+    /*
+    391.995f, // W G4 4
+    261.626f, // Y C4 1
+    349.228f, // G F4 3
+    523.251f, // B C5 6
+    293.665f, // R D4 2
+    440.000f  // O A4 5
+    */
+    // key -5
+    293.665f, // W D4 4
+    195.998f, // Y G3 1
+    261.626f, // G C4 3
+    391.995f, // B G4 6
+    220.000f, // R A3 2
+    329.628f  // O E4 5
+  },
+  {
+    // 民謡音階
+    391.995f, // W G4 4
+    261.626f, // Y C4 1
+    349.228f, // G F4 3
+    523.251f, // B C5 6
+    311.127f, // R Eb4 2
+    466.164f  // O Bb4 5
+  },
+  {
+    // 都節音階
+    /*
+    391.995f, // W G4 4
+    261.626f, // Y C4 1
+    349.228f, // G F4 3
+    523.251f, // B C5 6
+    277.183f, // R Db4 2
+    415.305f  // O Ab4 5
+    */
+    /*
+    // key -3
+    329.628f, // W E4 4
+    220.000f, // Y A3 1
+    293.665f, // G D4 3
+    440.000f, // B A4 6
+    233.082f, // R Bb3 2
+    349.228f  // O F4 5
+    */
+    // key -7
+    261.616f, // W C4 4
+    174.614f, // Y F3 1
+    233.082f, // G Bb3 3
+    349.228f, // B F4 6
+    184.997f, // R Gb3 2
+    277.183f  // O Db4 5
+  },
+  {
+    // 琉球音階
+    /*
+    391.995f, // W G4 4
+    261.626f, // Y C4 1
+    349.228f, // G F4 3
+    523.251f, // B C5 6
+    329.628f, // R E4 2
+    493.883f  // O B4 5
+    */
+    // key +3
+    466.164f, // W Bb4 4
+    311.127f, // Y Eb4 1
+    415.305f, // G Ab4 3
+    622.254f, // B Eb5 6
+    391.995f, // R G4 2
+    587.330f  // O D5 5
+  }
+};
+
+const int tone_buttons[N_TONES] = {23, 22, 21, 19, 18};
+
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> wOscil(SAW2048_DATA);
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> yOscil(SAW2048_DATA);
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> gOscil(SAW2048_DATA);
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> bOscil(SAW2048_DATA);
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> rOscil(SAW2048_DATA);
+Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> oOscil(SAW2048_DATA);
+Oscil<2048, 32768> *Oscils[N_FACES];
+
+ADSR <AUDIO_RATE, AUDIO_RATE> wenvelope;
+ADSR <AUDIO_RATE, AUDIO_RATE> yenvelope;
+ADSR <AUDIO_RATE, AUDIO_RATE> genvelope;
+ADSR <AUDIO_RATE, AUDIO_RATE> benvelope;
+ADSR <AUDIO_RATE, AUDIO_RATE> renvelope;
+ADSR <AUDIO_RATE, AUDIO_RATE> oenvelope;
+ADSR<32768, 32768> *envelopes[N_FACES];
+
+//ReverbTank reverb;
+
+LowPassFilter lpf;
+
+int f_values[N_FACES];
+int values[N_FACES];
+
+bool charging_led_state = false;
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  const char *SSID = "Slave_1";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+    Serial.print("AP CHANNEL "); Serial.println(WiFi.channel());
+  }
+}
+
+void set_freq(int idx){
+  for (int i = 0; i < N_FACES; ++i){
+    Oscils[i]->setFreq(tones[idx][i]);
+  }
+}
+
+void set_rgb_led_tone(int idx) {
+  for (int i = 0; i < 3; ++i) {
+    digitalWrite(rgb_led[i], !tone_led_colors[idx][i]);
+  }
+}
+
+void rgb_led_off() {
+  for (int i = 0; i < 3; ++i) {
+    digitalWrite(rgb_led[i], HIGH);
+  }
+}
+
+void rgb_led_light_bit(int bits) {
+  for (int i = 0; i < 3; ++i) {
+    bool bit = (bits >> i) & 1;
+    digitalWrite(rgb_led[i], !bit);
+  }
+}
+
+void setup() {
+  for (int i = 0; i < N_FACES; ++i){
+    f_values[i] = 0;
+    values[i] = 0;
+  }
+
+  for (int i = 0; i < N_TONES; ++i){
+    pinMode(tone_buttons[i], INPUT_PULLUP);
+  }
+  pinMode(CHARGING_LED, OUTPUT);
+  pinMode(AWAKE_LED, OUTPUT);
+  for (int i = 0; i < 3; ++i) {
+    pinMode(rgb_led[i], OUTPUT);
+  }
+  digitalWrite(AWAKE_LED, HIGH);
+  digitalWrite(CHARGING_LED, LOW);
+  rgb_led_off();
+
+  startMozzi(CONTROL_RATE);
+  Oscils[0] = &wOscil;
+  Oscils[1] = &yOscil;
+  Oscils[2] = &gOscil;
+  Oscils[3] = &bOscil;
+  Oscils[4] = &rOscil;
+  Oscils[5] = &oOscil;
+  envelopes[0] = &wenvelope;
+  envelopes[1] = &yenvelope;
+  envelopes[2] = &genvelope;
+  envelopes[3] = &benvelope;
+  envelopes[4] = &renvelope;
+  envelopes[5] = &oenvelope;
+  for (int i = 0; i < N_FACES; ++i){
+    envelopes[i]->setADLevels(255, 128);
+    envelopes[i]->setTimes(10, 10, 1000000, 200);
+  }
+  lpf.setCutoffFreqAndResonance(150, 100);
+  set_freq(tone_idx);
+  set_rgb_led_tone(tone_idx);
+
+  Serial.begin(115200);
+  Serial.println("ESPNow/Basic/Slave Example");
+
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  if (data[0] == 'C' && data[1] == 'u' && data[2] == 'Y' && data[3] == 'u'){
+    uint8_t datum = data[4];
+    if (datum == DATA_CHARGING) { // charging
+      for (int i = 0; i < N_FACES; ++i) {
+        if (f_values[i]) {
+          envelopes[i]->noteOff();
+          values[i] = 0;
+          f_values[i] = 0;
+        }
+      }
+      rgb_led_light_bit(led_blink_state);
+      ++led_blink_state;
+      if (led_blink_state >= 8) {
+        led_blink_state = 1;
+      }
+      charging_led_state = !charging_led_state;
+      digitalWrite(CHARGING_LED, charging_led_state);
+      awake_state = false;
+      digitalWrite(AWAKE_LED, awake_state);
+      Serial.println("Charging");
+    } else if (datum == DATA_DEEPSLEEP) { // sleep
+      awake_state = false;
+      digitalWrite(AWAKE_LED, awake_state);
+      rgb_led_off();
+      Serial.println("Sleep");
+    } else { // sound
+      if (charging_led_state) {
+        charging_led_state = false;
+        digitalWrite(CHARGING_LED, LOW);
+      }
+      if (!awake_state) {
+        awake_state = true;
+        digitalWrite(AWAKE_LED, awake_state);
+        set_rgb_led_tone(tone_idx);
+      }
+      for (int i = 0; i < N_FACES; ++i){
+        values[i] = (1 & (datum >> i));
+        if (values[i] == 1 && f_values[i] == 0){
+          envelopes[i]->noteOn();
+        } else if (values[i] == 0 && f_values[i] == 1){
+          envelopes[i]->noteOff();
+        }
+        f_values[i] = values[i];
+      }
+      
+      for (int i = 0; i < N_FACES; ++i){
+        uint8_t bit = 1 & (datum >> i);
+        Serial.print(bit);
+        Serial.print(' ');
+      }
+      Serial.println("");
+    }
+  }
+}
+
+void updateControl() {
+  for (int i = 0; i < N_TONES; ++i){
+    if (!digitalRead(tone_buttons[i])){
+      tone_idx = i;
+      set_freq(i);
+      set_rgb_led_tone(i);
+      Serial.print("tone set ");
+      Serial.println(i);
+    }
+  }
+}
+
+AudioOutput updateAudio(){
+  int synth = 0;
+  for (int i = 0; i < N_FACES; ++i){
+    envelopes[i]->update();
+    //synth += (values[i] * Oscils[i]->next() * envelopes[i]->next()) >> 8;
+    int gain = envelopes[i]->next();
+    if (values[i] == 1){
+      gain = max(gain, 128);
+    }
+    synth += (gain * Oscils[i]->next()) >> 8;
+  }
+  synth >> 3;
+  synth = lpf.next(synth)>>1;
+  return MonoOutput::fromAlmostNBit(9, synth);
+  //int arev = reverb.next(synth);
+  //return MonoOutput::fromAlmostNBit(9, synth + (arev >> 3));
+}
+
+void loop() {
+  audioHook();
+}
---
+++ CuYu3_1_QiaoKe/CuYu3_1_QiaoKe_main/CuYu3_1_QiaoKe_main.ino
@@ -0,0 +1,228 @@
+// original: ESPNOW sample code
+// modified by Nyanyan
+
+/**
+   ESPNOW - Basic communication - Master
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
+   Description: This sketch consists of the code for the Master module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+
+   << This Device Master >>
+
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave
+
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <esp_wifi.h> // only for esp_wifi_set_channel()
+
+// Global copy of slave
+esp_now_peer_info_t slave;
+#define CHANNEL 1
+#define PRINTSCANRESULTS 0
+#define DELETEBEFOREPAIR 0
+
+#define N_FACES 3
+// WGR
+uint8_t hall_data[N_FACES] = {0, 0, 0};
+const int hall_pin[N_FACES] = {D0, D2, D3}; // CuYu3.1 QiaoKe
+
+#define STATUS_SEND_SUCCESS 0
+#define STATUS_SEND_FAILED 1
+
+#define DATA_DEEPSLEEP 0b10000000
+#define DEEP_SLEEP_TIME_THRESHOLD 30000 // ms
+
+#define DATA_CHARGING 0b01000000
+#define CHARGING_LED D10
+#define BATTERY_CHARGING_N_THRESHOLD 10
+int n_battery_charging = 0;
+bool battery_charged = false;
+
+uint8_t last_hall_data_bit = 0b11111111;
+uint8_t hall_data_bit = 0;
+int data_status = STATUS_SEND_FAILED;
+unsigned long last_turned = 0;
+
+uint8_t send_data[5] = {'C', 'u', 'Y', 'u', 0};
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+void deep_sleep() {
+  Serial.println("sleep...");
+  esp_wifi_stop();
+  esp_deep_sleep_enable_gpio_wakeup(
+    BIT(GPIO_NUM_2) | BIT(GPIO_NUM_4) | BIT(GPIO_NUM_5), // D0 or D2 or D3
+    ESP_GPIO_WAKEUP_GPIO_LOW);
+  esp_deep_sleep_start();
+  Serial.println("wake");
+}
+
+void setup() {
+  for (int i = 0; i < N_FACES; ++i){
+    pinMode(hall_pin[i], INPUT_PULLUP);
+  }
+  pinMode(D0, INPUT_PULLUP); // for sleep
+  pinMode(D8, INPUT_PULLUP); // for boot
+  pinMode(CHARGING_LED, INPUT_PULLUP); // for charging
+  Serial.begin(115200);
+
+  //Set device in STA mode to begin with
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
+  // This is the mac address of the Master in Station Mode
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  Serial.print("STA CHANNEL "); Serial.println(WiFi.channel());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+
+  //const uint8_t slave_mac_addr[6] = {0x48, 0x31, 0xb7, 0x3f, 0xbe, 0x01};
+  const uint8_t slave_mac_addr[6] = {0x9c, 0x9c, 0x1f, 0xcf, 0xe8, 0x49};
+  memset(&slave, 0, sizeof(slave));
+  for (int i = 0; i < 6; ++i) {
+    slave.peer_addr[i] = slave_mac_addr[i];
+  }
+  while (true){
+    esp_err_t addStatus = esp_now_add_peer(&slave);
+    if (addStatus == ESP_OK) {
+      // Pair success
+      Serial.println("Pair success");
+      break;
+    } else{
+      Serial.println("Cannot Pair");
+    }
+  }
+
+  last_hall_data_bit = 0b11111111;
+  hall_data_bit = 0;
+  data_status = STATUS_SEND_FAILED;
+  last_turned = millis();
+}
+
+bool battery_charging() {
+  if (!digitalRead(CHARGING_LED)) {
+    if (n_battery_charging < BATTERY_CHARGING_N_THRESHOLD) {
+      ++n_battery_charging;
+    }
+    //Serial.println(n_battery_charging);
+    if (n_battery_charging >= BATTERY_CHARGING_N_THRESHOLD) {
+      return true;
+    }
+  } else {
+    n_battery_charging = 0;
+    //Serial.println(n_battery_charging);
+  }
+  return false;
+}
+
+void send() {
+  esp_err_t result = esp_now_send(slave.peer_addr, send_data, 5);
+  //esp_err_t result = esp_now_send(slave.peer_addr, &hall_data_bit, sizeof(hall_data_bit));
+  if (result == ESP_OK) {
+    data_status = STATUS_SEND_SUCCESS;
+    //Serial.println("Success");
+  } else {
+    data_status = STATUS_SEND_FAILED;
+    if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+      // How did we get so far!!
+      Serial.println("ESPNOW not Init.");
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
+      Serial.println("Invalid Argument");
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+      Serial.println("Internal Error");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+      Serial.println("Peer not found.");
+    } else {
+      Serial.println("Not sure what happened");
+    }
+  }
+}
+
+void loop() {
+  // charging
+  /*
+  if (battery_charging()) {
+    send_data[4] = DATA_CHARGING;
+    esp_err_t result = esp_now_send(slave.peer_addr, send_data, 5);
+    Serial.println("Charging");
+    delay(1000);
+    battery_charged = true;
+    return;
+  } else if (battery_charged) {
+    hall_data_bit = 0;
+    last_turned = millis();
+    send_data[4] = hall_data_bit;
+    send();
+    battery_charged = false;
+    return;
+  }
+  */
+
+  // update data
+  for (int i = 0; i < N_FACES; ++i){
+    hall_data[i] = !digitalRead(hall_pin[i]);
+  }
+
+  // send data
+  last_hall_data_bit = hall_data_bit;
+  hall_data_bit = 0;
+  for (int i = 0; i < N_FACES; ++i){
+    hall_data_bit |= hall_data[i] << (i * 2);
+  }
+  hall_data_bit |= hall_data_bit << 1;
+  if (hall_data_bit != last_hall_data_bit || data_status == STATUS_SEND_FAILED){
+    last_turned = millis();
+    
+    for (int i = 0; i < N_FACES; ++i){
+      Serial.print(hall_data[i]);
+    }
+    Serial.print(" ");
+    Serial.println(hall_data_bit);
+    
+    send_data[4] = hall_data_bit;
+    send();
+  } else if (hall_data_bit == 0 && millis() - last_turned > DEEP_SLEEP_TIME_THRESHOLD){ // sleep
+    send_data[4] = DATA_DEEPSLEEP;
+    send();
+    deep_sleep();
+  }
+
+  delay(10);
+}
